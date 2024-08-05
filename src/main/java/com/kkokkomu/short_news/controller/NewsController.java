8412b19b3020f8cc6b67874ea36d831c6c7ca459
Guyeon Chung <gouyeonch@naver.com>
2024-08-06 04:06:58 +0900

    [KKM-81] FEAT : 비로그인 숏폼 조회

--- src/main/java/com/kkokkomu/short_news/controller/NewsController.java
+++ src/main/java/com/kkokkomu/short_news/controller/NewsController.java
@@ -4,6 +4,7 @@ import com.kkokkomu.short_news.annotation.UserId;
 import com.kkokkomu.short_news.dto.common.PagingResponseDto;
 import com.kkokkomu.short_news.dto.common.ResponseDto;
 import com.kkokkomu.short_news.dto.news.response.GenerateNewsDto;
+import com.kkokkomu.short_news.dto.news.response.GuestNewsListDto;
 import com.kkokkomu.short_news.dto.news.response.NewsInfoDto;
 import com.kkokkomu.short_news.dto.news.response.NewsListDto;
 import com.kkokkomu.short_news.service.NewsService;
@@ -40,6 +41,13 @@ public class NewsController {
                                                                           @RequestParam int page, @RequestParam int size) {
         log.info("readNewsList controller");
         return ResponseDto.ok(newsService.readNewsList(userId, category, filter, page, size));
+    }
+
+    @Operation(summary = "비로그인 뉴스 리스트 조회")
+    @GetMapping("/list")
+    public ResponseDto<PagingResponseDto<List<GuestNewsListDto>>> guestReadNewsList(@RequestParam int page, @RequestParam int size) {
+        log.info("guestReadNewsList controller");
+        return ResponseDto.ok(newsService.guestReadNewsList(page, size));
     }
 
     @Operation(summary = "뉴스 정보 조회")
--- src/main/java/com/kkokkomu/short_news/service/NewsService.java
+++ src/main/java/com/kkokkomu/short_news/service/NewsService.java
@@ -134,6 +134,39 @@ public class NewsService {
         return PagingResponseDto.fromEntityAndPageInfo(newsListDtos, pageInfo);
     } // 숏폼 리스트 조회
 
+    public PagingResponseDto<List<GuestNewsListDto>> guestReadNewsList(int page, int size) {
+        // 일단 최신순으로 조회
+        Page<News> results = newsRepository.findAll(PageRequest.of(page, size));
+
+        List<News> news = results.getContent();
+        PageInfoDto pageInfo = PageInfoDto.fromPageInfo(results);
+
+        List<GuestNewsListDto> newsListDtos = new ArrayList<>();
+        for (News newsItem : news) {
+
+            // 뉴스 url 및 기본 정보
+            NewsSummaryDto newsSummaryDto = NewsSummaryDto.of(newsItem);
+
+            // 각 감정표현 별 갯수
+            ReactionCntDto reactionCntDto = ReactionCntDto.builder()
+                    .like(newsReactionRepository.countByNewsIdAndReaction(newsItem.getId(), ENewsReaction.LIKE))
+                    .angry(newsReactionRepository.countByNewsIdAndReaction(newsItem.getId(), ENewsReaction.ANGRY))
+                    .sad(newsReactionRepository.countByNewsIdAndReaction(newsItem.getId(), ENewsReaction.SAD))
+                    .surprise(newsReactionRepository.countByNewsIdAndReaction(newsItem.getId(), ENewsReaction.SURPRISE))
+                    .build();
+
+            // dto 생성
+            newsListDtos.add(
+                    GuestNewsListDto.builder()
+                            .shortformList(newsSummaryDto)
+                            .reactionCnt(reactionCntDto)
+                            .build()
+            );
+        }
+
+        return PagingResponseDto.fromEntityAndPageInfo(newsListDtos, pageInfo);
+    } // 비로그인 숏폼 리스트 조회
+
     // 비로그인 숏폼 리스트 조회
 
     @Transactional
