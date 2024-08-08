3c39f2ab4d215a0c30925cb99081df735ac5784f
Gouyeon Chung <gouyeonch@naver.com>
2024-08-08 15:14:42 +0900

    FEAT : 영상 생성 테스트 로직2

--- src/main/java/com/kkokkomu/short_news/controller/NewsController.java
+++ src/main/java/com/kkokkomu/short_news/controller/NewsController.java
@@ -26,10 +26,17 @@ public class NewsController {
     /************************** 홈화면 **************************/
 
     @Operation(summary = "뉴스 생성(이거 건들면안돼!!!!!!!!으어어ㅓ어)")
-    @PostMapping("")
+    @PostMapping("/test1")
     public ResponseDto<GenerateNewsDto> generateNews() {
         log.info("generateNews controller");
         return ResponseDto.ok(newsService.generateNews());
+    }
+
+    @Operation(summary = "뉴스 생성(이거 건들면안돼!!!!!!!!으어어ㅓ어)")
+    @PostMapping("/test2")
+    public ResponseDto<GenerateNewsDto> generateNews2() {
+        log.info("generateNews controller");
+        return ResponseDto.ok(newsService.generateNews2());
     }
 
     @Operation(summary = "뉴스 리스트 조회")
