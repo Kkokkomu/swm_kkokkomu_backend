f1f4a9d5a74f627eb2243cedeb660262069ab648
Gouyeon Chung <gouyeonch@naver.com>
2024-08-09 00:55:28 +0900

    [KKM-97] FEAT : 유저 카테고리 조회

--- src/main/java/com/kkokkomu/short_news/controller/UserCategoryController.java
+++ src/main/java/com/kkokkomu/short_news/controller/UserCategoryController.java
@@ -1,16 +1,16 @@
 package com.kkokkomu.short_news.controller;
 
+import com.kkokkomu.short_news.annotation.UserId;
 import com.kkokkomu.short_news.dto.userCategory.request.UpdateUserCategoryDto;
 import com.kkokkomu.short_news.dto.common.ResponseDto;
+import com.kkokkomu.short_news.dto.userCategory.response.CategoryByUserDto;
 import com.kkokkomu.short_news.service.UserCategoryService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "유저 카테고리")
 @Slf4j
@@ -25,5 +25,12 @@ public class UserCategoryController {
     public ResponseDto<String> updateUserCategory(@RequestBody UpdateUserCategoryDto updateUserCategoryDto) {
         log.info("update category : {}", updateUserCategoryDto);
         return ResponseDto.ok(userCategoryService.updateUserCategory(1L, updateUserCategoryDto));
+    }
+
+    @Operation(summary = "유저 카테고리 조회")
+    @GetMapping("")
+    public ResponseDto<CategoryByUserDto> readCategoryByUser(@Parameter(hidden = true) @UserId Long userId) {
+        log.info("update readCategoryByUser controller");
+        return ResponseDto.ok(userCategoryService.findUserCategoryByUserId(userId));
     }
 }
