4ae4e9a75433de622706911f8c49ab7554a78a3e
Gouyeon Chung <gouyeonch@naver.com>
2024-09-18 20:10:55 +0900

    FEAT : 뉴스 리스트 조회 transactional readonly 해제

--- src/main/java/com/kkokkomu/short_news/news/service/HomeNewsService.java
+++ src/main/java/com/kkokkomu/short_news/news/service/HomeNewsService.java
@@ -43,7 +43,7 @@ public class HomeNewsService {
     private final RedisService redisService;
 
     /* 홈화면 */
-    @Transactional(readOnly = true)
+    @Transactional
     public PagingResponseDto<List<NewsInfoDto>> readNewsList(Long userId, Long cursorId, int size) {
         User user = userLookupService.findUserById(userId);
 
@@ -78,7 +78,7 @@ public class HomeNewsService {
         return PagingResponseDto.fromEntityAndPageInfo(newsListDtos, pageInfo);
     } // 숏폼 리스트 조회
 
-    @Transactional(readOnly = true)
+    @Transactional
     public PagingResponseDto<List<GuestNewsInfoDto>> guestReadNewsList(Long cursorId, int size) {
         // 커서 아이디에 해당하는 뉴스가 있는지 검사
         if (cursorId != null && !newsRepository.existsById(cursorId)) {
