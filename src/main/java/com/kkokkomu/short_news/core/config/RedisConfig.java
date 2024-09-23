d46eba0f6ba2bbce44f59d589a0d61b7a171de9b
Gouyeon Chung <gouyeonch@naver.com>
2024-09-23 21:29:16 +0900

    FEAT : elasticach redis 설정

--- src/main/java/com/kkokkomu/short_news/core/config/RedisConfig.java
+++ src/main/java/com/kkokkomu/short_news/core/config/RedisConfig.java
@@ -3,10 +3,20 @@ package com.kkokkomu.short_news.core.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName("clustercfg.kkm-redis.rhiqa9.apn2.cache.amazonaws.com");
+        config.setPort(6379);
+        return new LettuceConnectionFactory(config);
+    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
