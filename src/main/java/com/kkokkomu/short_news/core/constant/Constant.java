cb77f37e56bb7084b9824e6fe0261d5b5e884e78
Gouyeon Chung <gouyeonch@naver.com>
2024-09-30 17:38:17 +0900

    FIX : 비디오 서버 host

--- src/main/java/com/kkokkomu/short_news/core/constant/Constant.java
+++ src/main/java/com/kkokkomu/short_news/core/constant/Constant.java
@@ -15,7 +15,7 @@ public class Constant {
     public static final String CONTENT_TYPE = "Content-Type";
     public static final Long REPLY_WEIGHT = 3L;
     public static final Long LIKE_WEIGHT = 2L;
-    public static final String VIDEO_SERVER_GENERATE_HOST = "http://3.38.99.39:8000/generate";
+    public static final String VIDEO_SERVER_GENERATE_HOST = "http://52.78.223.96:8000/generate";
     public static final Long MEMBER_INFO_RETENTION_PERIOD = 30L;
     public static final List<String> NO_NEED_AUTH_URLS = List.of(
             //테스트
