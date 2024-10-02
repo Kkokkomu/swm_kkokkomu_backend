c7835c06e6c4bf6b060429139a41208f04b6e2ea
Gouyeon Chung <gouyeonch@naver.com>
2024-10-03 03:47:36 +0900

    [KKM-152] FEAT : 뉴스 생성 시 카테고리별 랭킹보드 초기화

--- src/main/java/com/kkokkomu/short_news/news/service/AdminNewsService.java
+++ src/main/java/com/kkokkomu/short_news/news/service/AdminNewsService.java
@@ -107,6 +107,9 @@ public class AdminNewsService {
         log.info("response data : {}", (Object) Objects.requireNonNull(response.getBody()));
         GenerateResponseDto[] generateResponseDtos = response.getBody();
 
+        // 레디스 글로벌 랭킹 초기화
+        redisService.normalizeScores();
+
         // 영상 생성 서버에서 영상 url 및 정보 받아옴
         ObjectMapper objectMapper = new ObjectMapper();
         List<GenerateNewsDto> generateNewsDtos = new ArrayList<>();
@@ -156,8 +159,7 @@ public class AdminNewsService {
                     category
             );
 
-            // 레디스 랭킹 초기화
-            redisService.normalizeScores();
+            redisService.normalizeCategoryScores(news.getCategory());
 
             news = newsRepository.save(news);
 
