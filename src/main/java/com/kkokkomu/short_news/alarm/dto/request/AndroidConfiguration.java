bdcd41e2e9c81b2f2e574ed32410d5c700cb2271
Gouyeon Chung <gouyeonch@naver.com>
2024-10-30 06:00:41 +0900

    [KKM-161] FEAT : 알람 테스트

--- src/main/java/com/kkokkomu/short_news/alarm/dto/request/AndroidConfiguration.java
+++ src/main/java/com/kkokkomu/short_news/alarm/dto/request/AndroidConfiguration.java
@@ -2,18 +2,19 @@ package com.kkokkomu.short_news.alarm.dto.request;
 
 import com.google.firebase.messaging.AndroidConfig;
 import com.google.firebase.messaging.AndroidNotification;
+import com.kkokkomu.short_news.core.type.EAndroidChannelId;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AndroidConfiguration {
 
-    public AndroidConfig androidConfig(String channelId) {
+    public AndroidConfig androidConfig(EAndroidChannelId channelId) {
         return AndroidConfig.builder()
                 .setPriority(AndroidConfig.Priority.HIGH)
                 .setNotification(AndroidNotification.builder()
                         .setSound("default")
-                        .setChannelId(channelId)
+                        .setChannelId(channelId.toString().toLowerCase())
                         .build())
                 .build();
     }
