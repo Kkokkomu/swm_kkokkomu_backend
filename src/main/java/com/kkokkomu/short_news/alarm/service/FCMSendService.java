df05845c50827b235728b5b4ee3e108b2d689e59
Gouyeon Chung <gouyeonch@naver.com>
2024-11-11 01:28:35 +0900

    [KKM-161] FIX : FCMSendService 자가 참조 해제

--- src/main/java/com/kkokkomu/short_news/alarm/service/FCMSendService.java
+++ src/main/java/com/kkokkomu/short_news/alarm/service/FCMSendService.java
@@ -32,9 +32,6 @@ import java.util.List;
 @RequiredArgsConstructor
 @Slf4j
 public class FCMSendService {
-    @Autowired
-    private FCMSendService fcmSendService;
-
     @Value("${fcm.firebase-key}")
     private String firebaseConfigPath;
 
@@ -161,7 +158,4 @@ public class FCMSendService {
         }
         log.info("sendReplyAlarm 종료");
     }
-
-
-
 }
